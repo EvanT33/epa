6f2cc7be378a309ad8ac429f28784371a1d1334d
Evan Thompson <42624025+EvanT33@users.noreply.github.com>
2019-01-17 16:07:55 -0800

    Update epa.R
    new updates

--- epa.R
+++ epa.R
@@ -1,14 +1,8 @@
 # setwd
 setwd("C:/Users/v-evtho/Documents/Personal/NFL")
 
-
 # load packages
 library(nflscrapR)
-library(tidyverse)
-library(devtools)
-library(lubridate)
-library(stringr)
-
 
 # load 2018 play-by-play data
 season2018 <- season_play_by_play(Season = 2018)
@@ -16,6 +10,7 @@ write.csv(season2018, "season2018.csv", row.names = FALSE)
 season2018 <- read.csv("season2018.csv")
 save(season2018, file = "season2018.RData")
 
+# can start from here
 # load 2018
 load("season2018.RData")
 
@@ -25,12 +20,12 @@ summary(season2018$hawks)
 sea2018 <- season2018[ which(season2018$hawks == 1),]
 save(sea2018, file = "sea2018.RData")
 
+# keep desired variables
+myvars <- c("Date", "GameID", "Drive", "qtr", "down", "TimeSecs", "yrdline100", "ydstogo", "GoalToGo", "Yards.Gained", "FirstDown", "posteam", "HomeTeam", "AwayTeam", "desc", "Touchdown", "Safety", "PlayType", "RushAttempt", "FieldGoalResult", "PosTeamScore", "DefTeamScore", "ScoreDiff", "AbsScoreDiff", "EPA")
+sea2018 <- sea2018[myvars]
 
-
-# keep Seahawks data only
-season2018$hawks <- ifelse(season2018$PlayType == "Field Goal",1,0)
-summary(season2018$hawks)
-sea2018 <- season2018[ which(season2018$hawks == 1),]
+# keep desired PlayTypes
+sea2018$DesiredPlayType <- ifelse(sea2018$PlayType == "Run" | sea2018$PlayType == "Pass" | sea2018$PlayType == "Field Goal" | sea2018$PlayType == "Sack",1,0)
+sea2018 <- sea2018[ which(sea2018$DesiredPlayType == 1),]
+summary(season2018$PlayType)
 save(sea2018, file = "sea2018.RData")
-
-
